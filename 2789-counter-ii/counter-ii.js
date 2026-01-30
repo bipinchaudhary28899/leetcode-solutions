e3dae002d1284d4b96ba8e566687298635b537a0
Bipin Chaudhary <45562225+bipinchaudhary28899@users.noreply.github.com>
2026-01-30 11:31:13 +0530

    Time: 48 ms (70.07%) | Memory: 56.7 MB (47.94%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} init
+ * @return {{ increment: Function, decrement: Function, reset: Function }}
+ */
+var createCounter = function(init) { 
+    let count = init;
+    return {
+        increment: function() {
+            return ++count;
+        },
+        decrement: function() {
+            return --count;
+        },
+        reset: function() {
+            count = init;
+            return count;
+        }
+    };
+};
