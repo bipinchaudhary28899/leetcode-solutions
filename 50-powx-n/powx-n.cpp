5f333a6431dba55aa0c3c11fce9bbfa44110b856
Bipin Chaudhary <45562225+bipinchaudhary28899@users.noreply.github.com>
2026-02-03 10:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (13.89%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;        // promote to long long
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        if (N == 0) return 1.0;
+
+        double half = myPow(x, N / 2);
+
+        if (N % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+};
