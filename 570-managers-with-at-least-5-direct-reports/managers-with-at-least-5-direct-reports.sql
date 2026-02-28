065cfb96368275c26f6132445b35c859f07a10b5
Bipin Chaudhary <45562225+bipinchaudhary28899@users.noreply.github.com>
2026-02-28 11:04:43 +0530

    Time: 340 ms (55.96%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,10 @@
-SELECT name
-FROM Employee
-WHERE id IN (
+SELECT e.name
+FROM Employee e
+JOIN (
     SELECT managerId
     FROM Employee
     WHERE managerId IS NOT NULL
     GROUP BY managerId
     HAVING COUNT(*) >= 5
-);
+) m
+ON e.id = m.managerId;
