28e14060213e65d0919a2bb5b5c43352e8c92237
Bipin Chaudhary <45562225+bipinchaudhary28899@users.noreply.github.com>
2026-04-16 01:10:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (68.25%) - LeetSync

---
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+    int n = words.size();
+    int minDist = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (words[i] == target) {
+            int diff = abs(i - startIndex);
+            int dist = min(diff, n - diff);  // circular distance
+            minDist = min(minDist, dist);
+        }
+    }
+
+    return (minDist == INT_MAX) ? -1 : minDist;
+}
+};
